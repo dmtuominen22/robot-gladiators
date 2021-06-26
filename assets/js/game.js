ba2a17c60c5c0d58846474d84f1b9168f9b3cbc5
Denise <dmtuominen@gmail.com>
2021-06-26 17:23:34 -0600

    added robot name and console log to store it

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("our robot's name is " + playerName);
+
+//this creats a function named "fight"
+function fight () {
+    window.alert ("the fight has begun!");
+}
+
+//fight ();
